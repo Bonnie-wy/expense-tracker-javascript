f648f1cb911aacdc6e4f9687d4f1b471f2ca149a
Bonnie-wy <waiyeewong89@gmail.com>
2021-10-06 12:59:47 +0100

    script added to display balances

--- script.js
+++ script.js
@@ -20,8 +20,6 @@ function addTransactionDOM(transaction) {
     // Get sign
     const sign = transaction.amount < 0 ? '-' : '+';
 
-    // class="minus">Cash <span>-$400</span><button class="delete-btn">x</button></li>
-
     const item = document.createElement('li');
 
     // Add class based on value
@@ -35,11 +33,35 @@ function addTransactionDOM(transaction) {
     list.appendChild(item);
 }
 
+// Update the balance, income and expense
+function updateValues() {
+    const amounts = transactions.map(transaction => transaction.amount);
+
+    const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+
+    const income = amounts
+                        .filter(item => item > 0)
+                        .reduce((acc, item) => (acc += item), 0)
+                        .toFixed(2);
+
+    console.log(income);
+
+    const expense = (amounts
+                        .filter(item => item < 0)
+                        .reduce((acc, item) => (acc += item), 0) * -1)
+                        .toFixed(2);
+    balance.innerText = `$${total}`;
+    money_plus.innerText = `$${income}`;
+    money_minus.innerText = `$${expense}`;
+}
+
 // Init app
 function init() {
     list.innerHTML = '';
 
     transactions.forEach(addTransactionDOM);
+    updateValues();
 }
 
 init();
+
